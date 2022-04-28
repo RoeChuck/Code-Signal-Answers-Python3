a917e9aaacd42dbaf55b3d7f933144de8fd1c6d8
Rochak Baral <rowr.chuck@gmail.com>
2022-04-28 11:17:49 -0400

    Update almostIncreasingSequence.py
    better solution

--- Intro/almostIncreasingSequence.py
+++ Intro/almostIncreasingSequence.py
@@ -16,3 +16,24 @@ solution(sequence) = true.
 
 You can remove 3 from the array to get the strictly increasing sequence [1, 2]. Alternately, you can remove 2 to get the strictly increasing sequence [1, 3].
 """
+# keep track of the running max (m) and previous max (pm) from sequence, and number of mistakes
+# if mistakes > 1 during iteration, return False, exiting loop, saving time? See Big O notation for more information
+
+def solution(sequence):
+    cnt = 0
+    m = pm = float('-infinity') # set to infinity to account for the sequence starting at a very negative value
+    for e in sequence:
+        if e > m:
+            pm, m = m, e # first iteration should make pm = infinity (no change) and m = e
+        # if the current iteration value is not greater than the running max,
+        elif e > pm: # check if current iteration value is greater than the previous max 
+            m = e # sets new max to the running max
+            cnt += 1 # increment mistakes counter
+        else:
+            cnt += 1
+        # if at any point during iteration cnt is greater than 1, return false  
+        if cnt > 1:
+            return False
+    return True
+
+# See alt solution branch for more a worse but working solution
