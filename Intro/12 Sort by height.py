ed4e988dcf47530e1e621f92c587d811ba2e7acf
Rochak Baral <rowr.chuck@gmail.com>
2022-04-28 16:23:56 -0400

    Create 12 Sort by height.py

---
+++ Intro/12 Sort by height.py
@@ -0,0 +1,18 @@
+# Sort by height
+
+"""
+Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to 
+rearrange the people by their heights in a non-descending order without moving the trees. People can be very tall!
+
+Example
+
+For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output should be
+solution(a) = [-1, 150, 160, 170, -1, -1, 180, 190].
+"""
+
+def solution(inputArray):
+    ans = sorted([a for a in inputArray if a != -1])
+    [ans.insert(i, -1) for i, v in enumerate(inputArray) if v == -1]
+    return ans
+
+# Am I using list comprehension too much? Maybe, but it's concise and readable
