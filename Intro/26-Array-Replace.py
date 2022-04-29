bbcb7a74434efaa2ac09c602a9b750975b9eba7d
Rochak Baral <rowr.chuck@gmail.com>
2022-04-29 18:11:04 -0400

    delete duplicate

--- Intro/26-Array-Replace.py
+++
@@ -1,9 +0,0 @@
-# Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.
-
-# Example
-
-# For inputArray = [1, 2, 1], elemToReplace = 1, and substitutionElem = 3, the output should be
-# solution(inputArray, elemToReplace, substitutionElem) = [3, 2, 3].
-
-def solution(inputArray, elemToReplace, substitutionElem):
-    return [a if not a == elemToReplace else substitutionElem for a in inputArray ]
