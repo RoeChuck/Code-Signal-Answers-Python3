ef4cb414020a4c2f63173cc237e007ba7a67a7f1
Chuck Roe <rawr.chuck@gmail.com>
2022-05-04 14:16:13 -0400

    add time to test

--- Intro/13-reverseInParentheses.py
+++ Intro/13-reverseInParentheses.py
@@ -12,10 +12,11 @@
 
 # My solution:
 
-from turtle import st
+import time
 
 
 def sln1(inS):
+    start_time = time.time()
     input_list = list(inS)
     ans = []
     for i in inS:
@@ -27,20 +28,23 @@ def sln1(inS):
             ans.extend(temp_stack)
         else:
             ans.append(i)
-    return "".join(ans)
+    end_time = time.time()
+    return ("".join(ans), end_time - start_time)
 
 # there are multiple ways to do this, you could also check for occurance of '(' and set a start index and ')' as end
 # index and flip, and use recursion to solve it. It would look something like this (not my solution):
 
 
 def sln2(s):
+    start_time = time.time()
     for i, v in enumerate(s):
         if v == "(":
             start = i
         if v == ")":
             end = i
             return sln2(s[:start] + s[start+1:end][::-1] + s[end+1:])
-    return s
+    end_time = time.time()
+    return (s, end_time-start_time)
 
-
-print(sln2("fsf(fsiftdfjd(sdfsqf(fdsedf)fysfd)fsdfsdf)fdshsg"))
+# sln2 is has better time complexity, using the time module to verify
+# print(sln2("fsf(fsiftdfjd(sdfsqf(fdsedf)fysfd)fsdfsdf)fdshsg"))
